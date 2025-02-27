65675365c67840ef5f081b647f86e1d4215eb3e4
k2th0912 <k.kato@prosper-s.co.jp>
2025-02-27 18:29:45 +0900

    画像ファイル一式アップロード

--- src/Main.js
+++ src/Main.js
@@ -42,6 +42,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
+        fontSize:30
     },
 
     image: {
@@ -52,7 +53,8 @@ const styles = StyleSheet.create({
       },
 
       typography: {
-        color: 'white'
+        color: 'white',
+        fontSize:30
       },
 
       button: {
